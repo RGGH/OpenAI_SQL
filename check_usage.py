3d4e973928bb0db007d736f802ba00bb4a464b67
Red and Green <62441426+RGGH@users.noreply.github.com>
2023-05-28 14:57:12 +0100

    Update check_usage.py

--- check_usage.py
+++ check_usage.py
@@ -1,11 +1,23 @@
+"""
+Check token usage of OpenAI and multiple by corresponding model price
+https://redandgreen.co.uk
+Python 360 ~ https://www.youtube.com/@python360
+"""
+
 
 # Set your OpenAI API key
-api_key = "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"
+api_key = "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"
 
 import requests
 from datetime import date
 from pprint import pprint
-# Set your OpenAI API key
+
+
+snapshot_id = {
+  "text-davinci:003": 0.002,
+  "text-embedding-ada-002-v2": 0.02,
+}
+
 
 # Set the API endpoint for usage
 usage_url = "https://api.openai.com/v1/usage"
@@ -18,9 +30,13 @@ headers = {
 
 # Set the query parameters
 #today = date.today().isoformat() 
-
 params = {
     "date":"2023-05-24" 
+}
+
+pricing = {
+  "text-davinci:003": 0.02,
+  "text-embedding-ada-002-v2": 0.0004,
 }
 
 # Make the GET request to retrieve usage information
@@ -28,23 +44,25 @@ response = requests.get(usage_url, headers=headers, params=params)
 
 # Check if the request was successful
 if response.status_code == 200:
-    # Print the usage information
-# Extract the response JSON
+    # Extract the response JSON
     usage_data = response.json()
 
-    # Initialize the token count
-    token_count = 0
+    list =[] # create empty list
+    for val in usage_data['data']: 
+        cost = ((
+            val['n_context_tokens_total'] *  pricing[val['snapshot_id']]
+            +
+            val['n_generated_tokens_total'] *  pricing[val['snapshot_id']]
+            ) * 0.001)
 
-    # Iterate over each item in the usage data
-    for item in usage_data["data"]:
-        # Add up the token count
-        token_count += (item["n_generated_tokens_total"] + item['n_context_tokens_total'])
 
-    # Print the total token count
-    print("Total Token Count:", token_count)
-    print(f"Cost = ", token_count * 0.02 * 0.001)
+        list.append(cost)
+
+    print (sum(list))
+    
 else:
     # Print the error message if the request failed
     print(f"Request failed with status code: {response.status_code}")
-    print(response.json())
+
+
 
